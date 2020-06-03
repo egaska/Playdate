8cb5be547d14e804b1c221e59a522c1141e69dd2
Emily Gaska <emilygaska@gmail.com>
2020-06-03 12:17:37 -0500

    Added initial HTML for the activity.html page

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ module.exports = function(sequelize, DataTypes) {
   // In this case, before a User is created, we will automatically hash their password
   User.addHook("beforeCreate", user => {
     user.password = bcrypt.hashSync(
-      user.password,
+      user.password, 
       bcrypt.genSaltSync(10),
       null
     );
