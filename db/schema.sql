ac1f8825106cd44e641a5936ab299a9994b60307
Jonathan Alpart <panjialang@gmail.com>
2020-06-03 12:14:34 -0500

    schema visual in new assets folder

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE users(
+  id INT NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE events(
+  id INT NOT NULL AUTO_INCREMENT,
+  event_name VARCHAR(120) NOT NULL,
+  start_time INT NOT NULL,
+  end_time INT NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
