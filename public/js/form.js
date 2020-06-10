7dbce5b045b1f04725232fc0cc79d5c29beeaab3
Jonathan Alpart <panjialang@gmail.com>
2020-06-09 21:16:44 -0500

    logout button

--- public/js/form.js
+++ public/js/form.js
@@ -4,11 +4,18 @@ $(document).ready(() => {
     let newAvailablity = [...$(".custom-select")].map(
       (item) => "true" === item.value
     );
-  
+
     console.log(newAvailablity);
-    $.post("/api/events", {Availablity: newAvailablity}).then(function(data) {
-    console.log(data);
+    $.post("/api/events", { Availablity: newAvailablity }).then(function (data) {
+      console.log(data);
     });
     window.location.href = "/schedules";
+  });
+  $(".logout").on("click", (event) => {
+    event.preventDefault();
+    $.get("/logout").then(function (data) {
+      console.log('clicked logout');
+    });
+    window.location.href = "/";
   });
 });
